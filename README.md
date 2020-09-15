bd7de5a834b8f543075c7cd3cc18a5807dd3927a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-09-16 00:47:15 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Retro Memory Viewer
+
+Retro Memory Viewer is a GUI application that visualizes, in realtime, the data structures in memory of Metroid Prime 2: Echoes running in Dolphin.
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+
+
+with open("README.md") as readme_file:
+    long_description = readme_file.read()
+
+
+
+setup(
+    name='retro-memory-viewer',
+    author='Henrique Gemignani',
+    url='https://github.com/henriquegemignani/retro-memory-viewer',
+    description='A realtime memory viewer for the randomizer for the Metroid Prime 2: Echoes.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    scripts=[
+    ],
+    package_data={
+    },
+    license='License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Games/Entertainment',
+    ],
+    python_requires=">=3.7",
+    setup_requires=[
+    ],
+    install_requires=[
+        'dolphin-memory-engine>=1.0.2',
+    ],
+    extras_require={
+    },
+    entry_points={
+    },
+)
