3ce883530609b01b8cf2ec68908064f985324bb9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-09 04:02:18 +0200

    Enums, inactive new item buttons, fix recursion error

--- dread_editor/type_render.py
+++ dread_editor/type_render.py
@@ -26,6 +26,11 @@ def render_float_vector(value, path: str):
     return functions[len(value)](f"##{path}", *value)
 
 
+def render_typed_value(value, path: str):
+    imgui.text(str(value))
+    return False, value
+
+
 KNOWN_TYPE_RENDERS: dict[str, typing.Callable[[typing.Any, str], tuple[bool, typing.Any]]] = {
     "bool": render_bool,
     "float": render_float,
@@ -39,6 +44,7 @@ KNOWN_TYPE_RENDERS: dict[str, typing.Callable[[typing.Any, str], tuple[bool, typ
     "base::global::CRntString": render_string,
     "base::global::CName": render_string,
     "base::core::CAssetLink": render_string,
+    "base::reflection::CTypedValue": render_typed_value,
 
     "base::math::CVector2D": render_float_vector,
     "base::math::CVector3D": render_float_vector,
@@ -51,7 +57,7 @@ weak_ptr_re = re.compile(r"base::global::CWeakPtr<(.*)>$")
 raw_ptr_re = re.compile(r"(.*?)(?:[ ]?const)?\*$")
 ref_re = re.compile(r"CGameObjectRef<(.*)>$")
 typed_var_re = re.compile(r"(base::reflection::CTypedValue)$")
-all_ptr_re = [unique_ptr_re, weak_ptr_re, raw_ptr_re, ref_re, typed_var_re]
+all_ptr_re = [unique_ptr_re, weak_ptr_re, raw_ptr_re, ref_re]
 
 
 def find_ptr_match(type_name: str):
@@ -70,6 +76,14 @@ def render_vector_of_type(value: list, type_name: str, path: str):
         if changed:
             value[i] = result
             modified = True
+
+    imgui.separator()
+    with imgui.styled(imgui.STYLE_ALPHA, 0.5):
+        imgui.button("New Item")
+        imgui.next_column()
+        imgui.text("(Not Implemented)")
+        imgui.next_column()
+
     return modified, value
 
 
@@ -82,6 +96,13 @@ def render_dict_of_type(value: dict, type_name: str, path: str):
                 value[key] = result
                 modified = True
             imgui.tree_pop()
+
+    with imgui.styled(imgui.STYLE_ALPHA, 0.5):
+        imgui.button("New Item")
+        imgui.next_column()
+        imgui.text("(Not Implemented)")
+        imgui.next_column()
+
     return modified, value
 
 
@@ -92,6 +113,8 @@ def render_ptr_of_type(value, type_name: str, path: str):
 
 
 def render_value_of_type(value, type_name: str, path: str) -> tuple[bool, typing.Any]:
+    all_types: dict[str, typing.Any] = dread_data.get_raw_types()
+
     if type_name in KNOWN_TYPE_RENDERS:
         return KNOWN_TYPE_RENDERS[type_name](value, path)
 
@@ -111,7 +134,7 @@ def render_value_of_type(value, type_name: str, path: str) -> tuple[bool, typing
             nonlocal modified
 
             if type_data["parent"] is not None:
-                render_type(dread_data.get_raw_types()[type_data["parent"]])
+                render_type(all_types[type_data["parent"]])
 
             for field_name, field_type in type_data["fields"].items():
                 imgui.checkbox(f"##{path}.{field_name}_present", field_name in value)
@@ -130,16 +153,26 @@ def render_value_of_type(value, type_name: str, path: str) -> tuple[bool, typing
                                                                       f"{path}.{field_name}")
                         imgui.next_column()
 
-                    elif field_name.startswith("e"):
+                    elif all_types.get(field_type, {}).get("values") is not None:
+                        all_enum_values = list(all_types[field_type]["values"].keys())
                         imgui.text(field_name)
                         imgui.next_column()
-                        imgui.text(f"{value[field_name]}")
+                        changed, selected = imgui.combo(f"##{path}.{field_name}",
+                                                        all_enum_values.index(value[field_name]),
+                                                        all_enum_values)
+                        if changed:
+                            new_field = all_enum_values[selected]
                         imgui.next_column()
                     else:
-                        if imgui.tree_node(f"{field_name} ##{path}.{field_name}", imgui.TREE_NODE_DEFAULT_OPEN):
+                        node_open = imgui.tree_node(f"{field_name} ##{path}.{field_name}", imgui.TREE_NODE_DEFAULT_OPEN)
+                        if imgui.is_item_hovered():
+                            imgui.set_tooltip(field_type)
+
+                        if node_open:
                             changed, new_field = render_value_of_type(value[field_name], field_type,
                                                                       f"{path}.{field_name}")
                             imgui.tree_pop()
+                            
                         imgui.next_column()
                         imgui.next_column()
                 else:
