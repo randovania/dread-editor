83b0402a99c2dd21d5f6a48457728812f0f3a822
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-13 19:04:18 +0200

    Very basic BMSAD visualizer

--- dread_editor/main_loop.py
+++ dread_editor/main_loop.py
@@ -6,9 +6,12 @@ from pathlib import Path
 from typing import Optional
 
 import OpenGL.GL as gl
+import construct
 import glfw
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
+from mercury_engine_data_structures import type_lib
+from mercury_engine_data_structures.formats import Bmsad
 from mercury_engine_data_structures.pkg_editor import PkgEditor
 from mercury_engine_data_structures.type_lib import BaseType
 
@@ -17,7 +20,15 @@ from dread_editor.level_data import LevelData
 from dread_editor.preferences import global_preferences, load_preferences, save_preferences
 from dread_editor.type_render import SpecificTypeRender, TypeTreeRender
 
-all_bmsad_actordefs = []
+PathsByDirectory = dict[str, typing.Union[str, "PathsByDirectory"]]
+
+all_bmsad: list[str] = []
+nested_bmsad: PathsByDirectory = {}
+all_bmsad_actordefs: list[str] = []
+
+open_bmsad: dict[str, Bmsad] = {}
+
+bmsad_tree_render = TypeTreeRender()
 
 
 def prompt_file(directory: bool):
@@ -85,6 +96,110 @@ def add_custom_type_renders(tree_render: TypeTreeRender):
     tree_render.specific_renders["base::core::CAssetLink"] = AssetLinkRender()
 
 
+def clean_single_item_directory(directory: PathsByDirectory) -> PathsByDirectory:
+    if isinstance(directory, str):
+        return directory
+
+    result = {}
+    for key, value in directory.items():
+        if isinstance(value, str):
+            result[key] = value
+        elif len(value) > 1:
+            result[key] = clean_single_item_directory(value)
+        else:
+            # the way split_directories works, they're never empty
+            assert len(value) > 0
+            nested_key, nested_value = list(value.items())[0]
+            result[f"{key}/{nested_key}"] = clean_single_item_directory(nested_value)
+
+    return result
+
+
+def split_directories(files: list[str]) -> PathsByDirectory:
+    result = {}
+
+    for item in files:
+        parent = result
+        for part in item.split("/"):
+            assert len(part) > 0
+            if part not in parent:
+                if part.endswith(".bmsad"):
+                    parent[part] = item
+                else:
+                    parent[part] = {}
+            parent = parent[part]
+
+    return clean_single_item_directory(result)
+
+
+def draw_open_bmsad(current_scale: float):
+    string_vector = type_lib.get_type("base::global::CRntVector<base::global::CStrId>")
+
+    for path, bmsad in list(open_bmsad.items()):
+        active = imgui.begin(path, True)[1]
+        if not active:
+            open_bmsad.pop(path)
+            imgui.end()
+            continue
+
+        imgui.columns(2, "bmsad details")
+        imgui.text("Name")
+        imgui.next_column()
+        imgui.text(bmsad.raw.name)
+        imgui.next_column()
+
+        imgui.text("Type")
+        imgui.next_column()
+        imgui.text(bmsad.raw.type)
+        imgui.next_column()
+
+        imgui.separator()
+        prop = bmsad.raw.property
+
+        imgui.text("Model Name")
+        imgui.next_column()
+        imgui.text(prop.model_name)
+        imgui.next_column()
+
+        node_open = imgui.tree_node("Sub Actors", imgui.TREE_NODE_DEFAULT_OPEN)
+        imgui.next_column()
+        imgui.next_column()
+        if node_open:
+            changed, new_field = bmsad_tree_render.render_value_of_type(prop.sub_actors, string_vector, "sub_actors")
+            if changed:
+                prop.sub_actors = new_field
+            imgui.tree_pop()
+
+        node_open = imgui.tree_node("Components", imgui.TREE_NODE_DEFAULT_OPEN)
+        imgui.next_column()
+        imgui.next_column()
+        if node_open:
+            if isinstance(prop.components, construct.Container):
+                for component_key, component in prop.components.items():
+                    component_open = imgui.tree_node(component_key)
+                    imgui.next_column()
+                    imgui.next_column()
+                    if component_open:
+                        imgui.text("Type")
+                        imgui.next_column()
+                        imgui.text(component.type)
+                        imgui.next_column()
+
+                        if imgui.tree_node("Raw"):
+                            imgui.text(str(component))
+                            imgui.tree_pop()
+
+                        imgui.tree_pop()
+
+            imgui.tree_pop()
+
+        imgui.columns(1, "bmsad details")
+        if imgui.tree_node("Raw"):
+            imgui.text(str(bmsad.raw))
+            imgui.tree_pop()
+        imgui.end()
+
+
 def loop():
     imgui.create_context()
     window = impl_glfw_init()
@@ -107,14 +222,19 @@ def loop():
             if asset_name.endswith("brfld")
         ]
         possible_brfld.sort()
-        all_bmsad_actordefs.clear()
-        all_bmsad_actordefs.extend([
-            f"actordef:{asset_name}"
+        all_bmsad.clear()
+        all_bmsad.extend(
+            asset_name
             for asset_name in pkg_editor.all_asset_names()
             if asset_name.endswith("bmsad")
-        ])
+        )
+        all_bmsad.sort()
 
-        all_bmsad_actordefs.sort()
+        nested_bmsad.clear()
+        nested_bmsad.update(split_directories(all_bmsad))
+
+        all_bmsad_actordefs.clear()
+        all_bmsad_actordefs.extend(f"actordef:{asset_name}" for asset_name in all_bmsad)
 
         global_preferences["last_romfs"] = str(path)
         save_preferences()
@@ -175,6 +295,25 @@ def loop():
 
                 imgui.end_menu()
 
+            if imgui.begin_menu("BMSAD", len(nested_bmsad) > 0):
+                def recursive_draw_menu(nested):
+                    for key, value in nested.items():
+                        if isinstance(value, str):
+                            click, state = imgui.menu_item(key, "", value in open_bmsad)
+                            if click:
+                                if state:
+                                    open_bmsad[value] = pkg_editor.get_parsed_asset(value)
+                                else:
+                                    open_bmsad.pop(value)
+                        else:
+                            if imgui.begin_menu(key, True):
+                                recursive_draw_menu(value)
+                                imgui.end_menu()
+
+                recursive_draw_menu(nested_bmsad)
+
+                imgui.end_menu()
+
             imgui.end_main_menu_bar()
 
         if current_level_data is not None:
@@ -184,7 +323,9 @@ def loop():
         if current_level_data is not None:
             current_level_data.draw_visible_actors(current_scale)
 
-        # imgui.show_test_window()
+        draw_open_bmsad(current_scale)
+
+        imgui.show_test_window()
 
         gl.glClearColor(0, 0, 0, 1)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
