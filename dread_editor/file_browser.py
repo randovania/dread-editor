3816d8a695f5bf4bc558c96c048b9ec3060b3f28
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-12-05 23:11:38 +0200

    Implement filter

--- dread_editor/file_browser.py
+++ dread_editor/file_browser.py
@@ -3,6 +3,7 @@ import imgui
 
 class FileBrowser:
     _is_open: bool = False
+    filter: str = ""
 
     def __init__(self, all_files: list[str]):
         self.all_files_tree = {}
@@ -31,15 +32,35 @@ class FileBrowser:
             self._is_open = False
             return
 
-        def draw_tree(body: dict):
+        self.filter = imgui.input_text("Filter", self.filter, 500)[1]
+
+        def children_match_path(parent_path: str, body: dict) -> bool:
+            if self.filter in parent_path:
+                return True
+
             for name, contents in body.items():
                 if isinstance(contents, dict):
-                    if imgui.tree_node(name):
-                        draw_tree(contents)
+                    if children_match_path(f"{parent_path}/{name}/", contents):
+                        return True
+                elif self.filter in f"{parent_path}/{name}":
+                    return True
+
+            return False
+
+        def draw_tree(parent_path: str, body: dict):
+            for name, contents in body.items():
+                if isinstance(contents, dict):
+                    path = f"{parent_path}/{name}/"
+                    if not children_match_path(path, contents):
+                        continue
+
+                    if imgui.tree_node(name, imgui.TREE_NODE_DEFAULT_OPEN if len(contents) <= 1 else 0):
+                        draw_tree(path, contents)
                         imgui.tree_pop()
-                else:
+
+                elif self.filter in f"{parent_path}/{name}":
                     imgui.text(name)
 
-        draw_tree(self.all_files_tree)
+        draw_tree("", self.all_files_tree)
         imgui.end()
         return
